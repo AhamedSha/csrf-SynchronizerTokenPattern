63733417cea1ed567c4de2cbfe3a37423bca9d4b
Shadhir <ahamedshadhir11@gmail.com>
2018-09-17 14:33:42 +0530

    login_form

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
 <?php
-
+  
+  <html>
+   <head>
+       <title>Web Application</title>
+   </head>
+   <body>
+       <form>             
+           <div>
+             <label for="uname"><b>Username</b></label>
+             <input type="text" placeholder="Enter Username" name="uname" required>
+         
+             <label for="pwd"><b>Password</b></label>
+             <input type="password" placeholder="Enter Password" name="pwd" required>
+         
+             <button type="submit">Login</button>        
+           </div>
+         </form>
+   </body>
+   </html>
+   
+   
+   
 ?>
